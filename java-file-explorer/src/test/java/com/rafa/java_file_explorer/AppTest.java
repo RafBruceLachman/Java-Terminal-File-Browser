02863a3d0847f7637d5e4f85826f063f9e4fbbf5
RafBruceLachman <rafabruce98@gmail.com>
2023-04-07 01:07:50 -0300

    Created Maven Project

---
+++ java-file-explorer/src/test/java/com/rafa/java_file_explorer/AppTest.java
@@ -0,0 +1,20 @@
+package com.rafa.java_file_explorer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
